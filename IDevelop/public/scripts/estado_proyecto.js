84c5d238ccee74bb634155685af91bb87dbb485b
isma4102 <santoscastelvi2014@gmail.com>
2019-07-01 19:11:58 -0300

    errores

--- IDevelop/public/scripts/estado_proyecto.js
+++ IDevelop/public/scripts/estado_proyecto.js
@@ -5,28 +5,32 @@
 			$("#modalAviso").modal();
 		}
 		else{
-		var fechafinP = document.getElementById("txtFechaFP").value; 
-		var fechaEntregaP = document.getElementById("txtFechaE").value; 
-		
-		var retorno = Estado_proyecto_funcion(id,estado,fechafinP,fechaEntregaP);
-		if(retorno == "1"){
-			var mensaje = "Usted activo el proyecto correctamente";
-			$("#mensajeModal").html(mensaje);	
-			$("#modalAviso").modal();
-		}
-		else{
-			var mensaje = "Acción incorrecta";
-			$("#mensajeModal").html(mensaje);	
-			$("#modalAviso").modal();
-		}
-		var contenedor = document.getElementById('contenedor_carga');
-		contenedor.style.visibility = 'hidden';
-		contenedor.style.opacity = '0';
-	}
-}
+			var fechafinP = document.getElementById("txtFechaFP").value; 
+			var fechaEntregaP = document.getElementById("txtFechaE").value; 
 
-function Estado_proyecto_des(id,estado){
-	var retorno = Estado_proyecto_funcion_des(id,estado);
+			var retorno = Estado_proyecto_funcion(id,estado,fechafinP,fechaEntregaP);
+			if(retorno == "1" && estado == 4){
+				var mensaje = "Usted activo el proyecto correctamente";
+				$("#mensajeModal").html(mensaje);	
+				$("#modalAviso").modal();
+			}else if(retorno == '1' && estado == 0){
+				var mensaje = "Usted finalizó el proyecto correctamente";
+				$("#mensajeModal").html(mensaje);	
+				$("#modalAviso").modal();
+			}
+			else{
+				var mensaje = "Acción incorrecta";
+				$("#mensajeModal").html(mensaje);	
+				$("#modalAviso").modal();
+			}
+			var contenedor = document.getElementById('contenedor_carga');
+			contenedor.style.visibility = 'hidden';
+			contenedor.style.opacity = '0';
+		}
+	}
+
+	function Estado_proyecto_des(id,estado){
+		var retorno = Estado_proyecto_funcion_des(id,estado);
 		if(retorno == "1"){
 			var mensaje = "Usted desactivo el proyecto correctamente";
 			$("#mensajeModal").html(mensaje);	
@@ -40,7 +44,7 @@ function Estado_proyecto_des(id,estado){
 		var contenedor = document.getElementById('contenedor_carga');
 		contenedor.style.visibility = 'hidden';
 		contenedor.style.opacity = '0';
-}
+	}
 	function Estado_proyecto_funcion(id,estado,fechafinP,fechaEntregaP){
 		var retorno;
 		$.ajax({
@@ -65,7 +69,7 @@ function Estado_proyecto_des(id,estado){
 		return retorno;
 	}
 
-function Estado_proyecto_funcion_des(id,estado){
+	function Estado_proyecto_funcion_des(id,estado){
 		var retorno;
 		$.ajax({
 			async:false,
